a947f5328cc80a4390577706b597719945a60ede
cheal3 <49738628+cheal3@users.noreply.github.com>
2019-06-08 17:57:58 +0900

    3차

--- picam-client.py
+++ picam-client.py
@@ -10,7 +10,7 @@ port = "/dev/ttyACM0"
 ser = serial.Serial(port,9600)
 ser.flushInput()
 
-src = "/home/pi/tt/";  
+src = "/home/pi/tt/"  # 저장되는 디렉터리
 
 dte = time.localtime()
 Year = dte.tm_year
@@ -20,23 +20,9 @@ Hour = dte.tm_hour
 Min = dte.tm_min
 Sec = dte.tm_sec
 
-time = str(Year) + "-" + str(Mon)+"-" + str(Day)+" "+str(Hour)+":"+str(Min)
-time2 = str(Year)+"_"+str(Mon)+"_"+str(Day)+"_"+str(Hour)+"_"+str(Min)
+time = str(Year) + "-" + str(Mon)+"-" + str(Day)+" "+str(Hour)+":"+str(Min)  # 메시지에 보낼 시간 정보
+time2 = str(Year)+"_"+str(Mon)+"_"+str(Day)+"_"+str(Hour)+"_"+str(Min)  #파일명에 쓰일 시간
 
-def transfer(filename):
-    capture_file_name = src + str(filename) + ".jpg"
-    file = open(capture_file_name, "rb")
-    img_size = os.path.getsize(capture_file_name)
-    img = file.read(img_size)
-    file.close()
-
-    client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    client_socket.connect(("192.168.0.138", 50011))
-
-    client_socket.sendall(img)
-
-    client_socket.close()
-    print("Finish Send")
 
 
 def echo_client(server_addr):
@@ -50,21 +36,25 @@ def echo_client(server_addr):
             distance=[]  
             ser.write("in".encode('utf-8'))
             distance.append(float(ser.readline()))
-            s_msg = "type:load\r\ntime:%s\r\ndistance %s\r\n" %(time,distance[0])  
-            sock.send(s_msg.encode('utf-8'))
+
             if distance[0]<5 :
                 print('cam')
                 with picamera.PiCamera() as camera:
                     camera.start_preview(fullscreen=False, window=(100, 20, 640, 480))
                     camera.capture('/home/pi/tt/%s.jpg' % time2)
                     camera.stop_preview()
-                transfer(time2)
-                
-
+                capture_file_name = src + str(time2) + ".jpg" # 어떤 파일 형태로
+                file = open(capture_file_name, "rb")
+                data = file.read(8192)
+                while(data):
+                    sock.send(data)
+                    data = file.read(8192)
+                file.close()
+                print("send finished")
 	
-        data = sock.recv(1024).decode('utf-8')
-        #print(data,end='')
+
     sock.close()
 
 if __name__ == '__main__':
     echo_client(('192.168.0.63',50011))
+
