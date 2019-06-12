71d037b53ceab30a692f173e70bb32d7487432ce
cheal3 <49738628+cheal3@users.noreply.github.com>
2019-06-12 18:49:46 +0900

    최종

--- final-server.py
+++ final-server.py
@@ -1,25 +1,27 @@
 from socket import socket,AF_INET, SOCK_STREAM # 소켓 임포트
-import sqlite3
-import os
 import time
-import sys
 
 
 #이미지 파일 저장 위치
 src = "C:/Users/cheal/AndroidStudioProjects/xproject/app/src/main/res/drawable/"
 
-
-def fileName():
+# a를 넣고 부르면 이미지파일 저장 b를 넣고 부르면 text 파일에 저장
+def fileName(x):
     dte = time.localtime()
     Year = dte.tm_year
     Mon = dte.tm_mon
     Day = dte.tm_mday
-    WDay = dte.tm_wday
     Hour = dte.tm_hour
     Min = dte.tm_min
     Sec = dte.tm_sec
-    imgFileName = src +'d'+ str(Year) + '_' + str(Mon) + '_' + str(Day) + '_' + str(Hour) + '_' + str(Min) +'.jpg'
-    return imgFileName
+
+    textFile = str(Year) + '_' + str(Mon) + '_' + str(Day) + '_' + str(Hour) + '_' + str(Min)
+    imgFileName = src + textFile + '.jpg'
+
+    if x == 'a':
+        return imgFileName
+    elif x=='b':
+        return textFile
 
 def echo_server(my_port):
     sock = socket(AF_INET, SOCK_STREAM)  # 소켓 객체를 생성
@@ -30,22 +32,23 @@ def echo_server(my_port):
         conn, client_addr = sock.accept()  # 서버소켓에 클라이언트가 연결되면 클라이언트 소켓, 주소를 반환
         print('connected by', client_addr)  # 어떤 주소에서 연결되었는지 프린트
         try:
-            f = open(fileName(), 'wb')
+            f = open(fileName('a') , 'wb')  #시간정보를 받아와서 파일을 write binary로 open
 
             while True:
-                data = conn.recv(4096)
-                if not data:
+                data = conn.recv(4096)  #데이터를 512Byte씩 받아와서
+                if not data:  # 데이터가 오지 않을때 까지
                     break
                 else:
-                    f.write(data)
+                    f.write(data) # 파일을 작성
             f.close()
-            file = open(src + 'fileName.txt', 'a')
-            file.write(time.ctime() + '\n')
+            file = open(src + 'filename.txt', 'a') # 텍스트파일(LOG) 에
+            file.write(fileName('b') + '\n') # 현재 시간을 저장
             file.close()
 
-
-
-
+#'''    # wb모드로 open한 이유
+        # 바이너리 파일에는 파일의 형식에 관한 내용이 포함되기 때문에
+        # 바이너리 파일을 전송하면 파일 형식에 대한 언급 없이도 자동으로 JPG ,txt 파일을 만들 수 있다.
+        # 또한 소켓은 bytes 를 전달하기 때문에 별다른 디코딩 과정없이 파일에 작성해도 된다.'''#
 
 
 
@@ -61,3 +64,5 @@ def echo_server(my_port):
 
 if __name__ == '__main__':
     echo_server(50011)   # 포트번호
+
+
